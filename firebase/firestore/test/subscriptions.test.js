525268f0077b97962c7c04e08f6ed92d3952ea37
Dao Hoang Son <daohoangson@gmail.com>
2019-10-10 14:44:29 +0700

    Add `firestore.rules` to protect subscriptions data

---
+++ firebase/firestore/test/subscriptions.test.js
@@ -0,0 +1,45 @@
+const firebase = require("@firebase/testing");
+const fs = require("fs");
+
+const projectId = "blogspot-reader";
+const rules = fs.readFileSync("firestore.rules", "utf8");
+
+const _ = auth => firebase.initializeTestApp({ projectId, auth }).firestore();
+
+beforeEach(() => firebase.clearFirestoreData({ projectId: projectId }));
+before(() => firebase.loadFirestoreRules({ projectId: projectId, rules }));
+after(() => Promise.all(firebase.apps().map(app => app.delete())));
+
+describe("subscriptions", () => {
+  it("shouldn't accept write from guest", () =>
+    firebase.assertFails(
+      _(null)
+        .collection("subscriptions")
+        .doc("xxx")
+        .set({ foo: "bar" })
+    ));
+
+  it("shouldn't accept write from user", () =>
+    firebase.assertFails(
+      _({ uid: "user" })
+        .collection("subscriptions")
+        .doc("xxx")
+        .set({ foo: "bar" })
+    ));
+
+  it("shouldn't accept read from guest", () =>
+    firebase.assertFails(
+      _(null)
+        .collection("subscriptions")
+        .doc("xxx")
+        .get()
+    ));
+
+  it("shouldn't accept read from user", () =>
+    firebase.assertFails(
+      _({ uid: "user" })
+        .collection("subscriptions")
+        .doc("xxx")
+        .get()
+    ));
+});
