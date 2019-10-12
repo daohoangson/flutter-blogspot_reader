99babbda0af7bd24fe61c50130246380c7063276
Dao Hoang Son <daohoangson@gmail.com>
2019-10-12 23:02:12 +0700

    Implement Notification Service Extension for FCM image.
    Documents:
    
    - https://firebase.google.com/docs/cloud-messaging/ios/send-image
    - https://developer.apple.com/library/archive/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/ModifyingNotifications.html

---
+++ ios/FcmImage/NotificationService.m
@@ -0,0 +1,24 @@
+#import "NotificationService.h"
+#import <FirebaseMessaging/FirebaseMessaging.h>
+
+@interface NotificationService ()
+
+@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    self.contentHandler = contentHandler;
+    self.bestAttemptContent = [request.content mutableCopy];
+
+    [[FIRMessaging extensionHelper] populateNotificationContent:self.bestAttemptContent withContentHandler:contentHandler];
+}
+
+- (void)serviceExtensionTimeWillExpire {
+    self.contentHandler(self.bestAttemptContent);
+}
+
+@end
